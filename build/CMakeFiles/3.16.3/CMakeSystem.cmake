45b60affcb9788b9530b0308c1f44ec5bdb96f2e
Eron Castro <eroncastro@gmail.com>
2020-10-17 13:19:33 -0300

    Fix platformio

--- build/CMakeFiles/3.16.3/CMakeSystem.cmake
+++ build/CMakeFiles/3.16.3/CMakeSystem.cmake
@@ -1,6 +1,6 @@
-set(CMAKE_HOST_SYSTEM "Linux-5.4.0-48-generic")
+set(CMAKE_HOST_SYSTEM "Linux-5.4.0-51-generic")
 set(CMAKE_HOST_SYSTEM_NAME "Linux")
-set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-48-generic")
+set(CMAKE_HOST_SYSTEM_VERSION "5.4.0-51-generic")
 set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
 
 include("/home/eron/esp/esp-idf/tools/cmake/toolchain-esp32.cmake")
--- src/main.c
+++ src/main.c
@@ -1,15 +1,30 @@
+#include <string.h>
+#include <stdio.h>
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "sum.h"
 
+void callingFunc()
+{
+  ESP_LOGI("TASK1", "1 - In func");
+  char buffer[10];
+  ESP_LOGI("TASK1", "2 - In func");
+  memset(buffer, 'x', sizeof(buffer));
+  ESP_LOGI("TASK1", "3 - In func");
+  ESP_LOGI("BUFFER", "buffer is %s\n", buffer);
+}
+
+void task1(void *params)
+{
+  ESP_LOGI("TASK1", "Before func");
+  callingFunc();
+  ESP_LOGI("TASK1", "After func");
+  vTaskDelete(NULL);  
+}
+
 void app_main()
 {
-  int i = 0;
-  while (1) {
-    int sumResult = sum(i++, 2);
-    ESP_LOGI("TAG1", "Hello, World!");
-    ESP_LOGI("SUM", "%d", sumResult);
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-  }
+  ESP_LOGI("MAIN", "EXECUTING TASK");
+  xTaskCreate(task1, "task1", 1024 * 2, NULL, 5, NULL);
 }
